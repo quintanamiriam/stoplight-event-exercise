4f0a8887ee09ccc3b50773527a6000705cff8a04
Miriam Quintana <quintana.miriam.cc@gmail.com>
2017-07-24 14:59:21 -0600

    stoplight exercise

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
 (function() {
   'use strict';
+  var button = document.getElementById("stopButton")
+  var light = document.getElementById("stopLight")
+  console.log(button)
+  button.addEventListener("click", function(){
+  light.style.backgroundColor = "red"});
 
-  // YOUR CODE HERE
-})();
+
+var button2 = document.getElementById("slowButton")
+var light2 = document.getElementById("slowLight")
+console.log(button2)
+button2.addEventListener("click", function(){
+light2.style.backgroundColor = "yellow"})
+
+
+var button3 = document.getElementById("goButton")
+var light3 = document.getElementById("goLight")
+console.log(button3)
+button3.addEventListener("click", function(){
+light3.style.backgroundColor = "green"})
+
+}) ();
